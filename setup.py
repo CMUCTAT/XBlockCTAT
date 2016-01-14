4d346d113aa16565bc03ba4b90b42a1fe61e282e
vvelsen <vvelsen@cs.cmu.edu>
2016-01-14 13:00:24 -0500

    Time to update the version again because, you know, the server won't otherwise refresh.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def package_data(pkg, roots):
 
 setup(
     name='ctatxblock-xblock',
-    version='0.38',
+    version='0.40',
     description='CTAT XBlock Template',
     packages=[
         'ctatxblock',
