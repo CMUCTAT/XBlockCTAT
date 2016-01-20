7ad899c2eb3fd4b4f3a292744cff3b527f0758c2
vvelsen <vvelsen@cs.cmu.edu>
2016-01-20 13:42:11 -0500

    Changed the grading code in Js to normalize the grade on a 0 to 1 scale as shown in the documentation (just in case)

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def package_data(pkg, roots):
 
 setup(
     name='ctatxblock-xblock',
-    version='0.44',
+    version='0.45',
     description='CTAT XBlock Template',
     packages=[
         'ctatxblock',
