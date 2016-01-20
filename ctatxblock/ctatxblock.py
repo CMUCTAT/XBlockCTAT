bb18847fa11ce2147e50d3baf2faed78ebe45cee
vvelsen <vvelsen@cs.cmu.edu>
2016-01-20 17:25:59 -0500

    Grading test

--- ctatxblock/ctatxblock.py
+++ ctatxblock/ctatxblock.py
@@ -111,8 +111,10 @@ class CTATXBlock(XBlock):
         #self.max_score = data['max_value']
         #self.completed = self.score >= self.max_score
         #event_data = {'value': self.score, 'max_value': self.max_score}
-        event_data = {'value': self.score, 'max_value': 1.0}
-        self.runtime.publish(self, 'grade', event_data);
+        #event_data = {value : self.score, max_value : 1.0}
+        self.runtime.publish(self, "grade", 
+                            { value: 0.5
+                              max_value: 1.0 })
         return {'result': 'success'}
 
     @XBlock.json_handler
