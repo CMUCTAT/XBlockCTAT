f3597daa40c05730316fefd4dac447f0682a7c8a
vvelsen <vvelsen@cs.cmu.edu>
2016-01-21 10:55:52 -0500

    For our next and hopefully final trick save-and-restore has been enabled again.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def package_data(pkg, roots):
 
 setup(
     name='ctatxblock-xblock',
-    version='0.57',
+    version='0.58',
     description='CTAT XBlock Template',
     packages=[
         'ctatxblock',
