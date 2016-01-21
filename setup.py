7f1ffd483f199505c93613c5640c6e17da3b7191
vvelsen <vvelsen@cs.cmu.edu>
2016-01-21 12:24:27 -0500

    Started the fixing/exploration phase as to why our code isn't picking up the problem restore package.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def package_data(pkg, roots):
 
 setup(
     name='ctatxblock-xblock',
-    version='0.60',
+    version='0.61',
     description='CTAT XBlock Template',
     packages=[
         'ctatxblock',
