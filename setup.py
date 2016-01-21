d668c0365e808c4709711e7220cc71dc9547e753
vvelsen <vvelsen@cs.cmu.edu>
2016-01-21 12:34:11 -0500

    Reworked the initialization order a bit to make sure the XBlock code gets to check first if it needs to start a restore process.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def package_data(pkg, roots):
 
 setup(
     name='ctatxblock-xblock',
-    version='0.61',
+    version='0.62',
     description='CTAT XBlock Template',
     packages=[
         'ctatxblock',
