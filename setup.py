d704b18306326ec251e1132228012be0b90cbb31
vvelsen <vvelsen@cs.cmu.edu>
2016-01-21 12:41:56 -0500

    Fixed a typo in the restore code

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def package_data(pkg, roots):
 
 setup(
     name='ctatxblock-xblock',
-    version='0.62',
+    version='0.63',
     description='CTAT XBlock Template',
     packages=[
         'ctatxblock',
