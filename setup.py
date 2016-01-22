050a8715687aa63ec2c5df628fc94bce3978fb42
vvelsen <vvelsen@cs.cmu.edu>
2016-01-22 12:30:34 -0500

    Fixed a bug in when we load the language manager.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def package_data(pkg, roots):
 
 setup(
     name='ctatxblock-xblock',
-    version='0.74',
+    version='0.75',
     description='CTAT XBlock Template',
     packages=[
         'ctatxblock',
