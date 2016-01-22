eed756af68276035a275356e53ab0d80a3d5ac5f
vvelsen <vvelsen@cs.cmu.edu>
2016-01-22 13:22:29 -0500

    Change to problem restore.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def package_data(pkg, roots):
 
 setup(
     name='ctatxblock-xblock',
-    version='0.77',
+    version='0.78',
     description='CTAT XBlock Template',
     packages=[
         'ctatxblock',
