9e19094937150f02482427b6ff1f94a7b1005863
Ringenberg <Ringenberg@users.noreply.github.com>
2016-03-24 11:59:43 -0400

    Added some comments. CTATLMS.setValue() should now update the local cache as well as the database entries.

--- ctatxblock/public/js/CTATXConfig.js
+++ ctatxblock/public/js/CTATXConfig.js
@@ -1,17 +1,24 @@
 /*******
  * This is to be loaded after ctat.min.js but before calling initTutor in
  * the XBlock environment.
+ *
+ * window.parent.CTATConfig is defined in static/html/ctatxblock.html which
+ * is a template expanded and added by ctatxblock.py
+ *
+ * window.parent.CTATXBlock is defined in static/js/CTATXBlock.js which is
+ * added and executed by ctatxblock.py
  */
 
 /** set the environment */
-var CTATTarget="XBlock"; // this should eventually be removed
 var CTATXConfig = window.parent.CTATConfig; // grab from encompasing environment
-//setVariable = window.parent.CTATXBlock.post.set_variable;
-//getVariable = function(key) { return window.atob(CTATXConfig[key]); };
-//gradeStudent = window.parent.CTATXBlock.post.report_grade;
 
 CTATLMS.identifier = 'XBlock';
-CTATLMS.setValue = window.parent.CTATXBlock.post.set_variable;
+CTATLMS.setValue = function(key,value) {
+    // update "local" copy as well because xblock does not update the
+    // templated values durring runtime.
+    CTATXConfig[key] = value;
+    return window.parent.CTATXBlock.post.set_variable(key,value);
+};
 CTATLMS.getValue = function(key) { return CTATXConfig[key]; };
 CTATLMS.saveProblemState = function (state) {
     CTATLMS.setValue('saveandrestore',window.btoa(state));
