3bb5c50dd93d536f02b6ba17aa45dac33e6b1f54
Ringenberg <Ringenberg@users.noreply.github.com>
2016-09-13 09:48:13 -0400

    Harden the code against some maliciousness. Also some changes to satisfy pylint and pep8.

--- ctatxblock/ctatxblock.py
+++ ctatxblock/ctatxblock.py
@@ -5,16 +5,19 @@ See http://ctat.pact.cs.cmu.edu/ for more on CTAT tutors.
 
 import re
 import uuid
-import base64
+# import base64
 import math
+import json
 import pkg_resources
+import requests
+import bleach
 
 # pylint: disable=import-error
 # The xBlock package are availabe in the runtime environment.
 from xblock.core import XBlock
 from xblock.fields import Scope, Integer, String, Float, Boolean
 from xblock.fragment import Fragment
-#pylint: enable=import-error
+# pylint: enable=import-error
 
 
 class CTATXBlock(XBlock):
@@ -29,10 +32,10 @@ class CTATXBlock(XBlock):
         default="CTAT Tutor",
         scope=Scope.content)
     # **** xBlock tag variables ****
-    width = 690 #Integer(help="Width of the tutor frame.",
-                #    default=690, scope=Scope.content)
-    height = 550 #Integer(help="Height of the tutor frame.",
-                 #    default=550, scope=Scope.content)
+    width = Integer(help="Width of the tutor frame.",
+                    default=690, scope=Scope.content)
+    height = Integer(help="Height of the tutor frame.",
+                     default=550, scope=Scope.content)
 
     # **** Grading variables ****
     # Required accordign to EdX's documentation in order to get grading to work
@@ -43,7 +46,7 @@ class CTATXBlock(XBlock):
     max_problem_steps = Integer(
         help="Total number of steps",
         scope=Scope.user_state, default=1)
-    max_possible_score = 1 # should this be 1.0?
+    max_possible_score = 1  # should this be 1.0?
 
     def max_score(self):
         """ The maximum raw score of the problem. """
@@ -65,15 +68,15 @@ class CTATXBlock(XBlock):
     )  # weight needs to be set to something, errors will be thrown if it does
     # not exist.
 
-
     # **** Basic interface variables ****
     src = String(help="The source html file for CTAT interface.",
-                 #default="https://cdn.rawgit.com/CMUCTAT/CTAT/v4.0.0/Examples/FractionAddition.html",
-                 #default="http://localhost/~mringenb/postMessage.html",
+                 # default="https://cdn.rawgit.com/CMUCTAT/CTAT/v4.0.0" +
+                 # "/Examples/FractionAddition.html",
                  default="http://www.cs.cmu.edu/~mringenb/tutor/CTATNumberLine.html",
                  scope=Scope.settings)
     brd = String(help="The behavior graph.",
-                 #default="https://cdn.rawgit.com/CMUCTAT/CTAT/v4.0.0/Examples/FractionAddition.brd",
+                 # default="https://cdn.rawgit.com/CMUCTAT/CTAT/v4.0.0" +
+                 # "/Examples/FractionAddition.brd",
                  default="http://www.cs.cmu.edu/~mringenb/tutor/CTATNumberLine.brd",
                  scope=Scope.settings)
 
@@ -91,6 +94,12 @@ class CTATXBlock(XBlock):
     logtype = String(help="How should data be logged",
                      default="None", scope=Scope.settings)
 
+    custom_tutor_parameters = String(
+        help="Extra parameters set by the author for the tutor. " +
+        "This should be JSON code. See https://github.com/CMUCTAT/CTAT/" +
+        "wiki/Advanced-Topics#tutor-configuration.",
+        default="{}",
+        scope=Scope.settings)
     # **** User Information ****
     saveandrestore = String(help="Internal data blob used by the tracer",
                             default="", scope=Scope.user_state)
@@ -104,15 +113,6 @@ class CTATXBlock(XBlock):
         data = pkg_resources.resource_string(__name__, path)
         return data.decode("utf8")
 
-    @staticmethod
-    def strip_local(url):
-        """ Returns the given url with //localhost:port removed. """
-        return re.sub(r'//localhost(:\d*)?', '', url)
-
-    def get_local_resource_url(self, url):
-        """ Wrapper for self.runtime.local_resource_url. """
-        return self.strip_local(self.runtime.local_resource_url(self, url))
-
     # **** xBlock methods ****
     def student_view(self, dummy_context=None):
         """
@@ -123,22 +123,43 @@ class CTATXBlock(XBlock):
         Returns:
           a Fragment object containing the HTML to display.
         """
+        # pylint: disable=broad-except
+        # There are a fair number of exceptions raised by raise_for_status
+        # and all of them are handled the same way here.
+        try:
+            # See if tutor interface is reachable and add mode parameter
+            tutor = requests.get(self.src, params={'mode': 'XBlock'})
+            tutor.raise_for_status()
+        except Exception as err:
+            # Return a Fragment with the error message so that authors can
+            # get some relevant information as to why the tutor is not loading.
+            return Fragment((u'<p style="width:{width}px;height:{height}px;' +
+                             'border:2px solid red;">Error retrieving ' +
+                             'specified tutor {url} ({error})</p>').format(
+                                 url=self.src,
+                                 error=err,
+                                 height=self.height,
+                                 width=self.width))
+        # pylint: enable=broad-except
         # read in template html
         html = self.resource_string("static/html/ctatxblock.html")
+        # fill in the template html
         frag = Fragment(html.format(
-            tutor_html=self.src, # FIXME: add ?mode=XBlock in such a way as to handle other get parameters
+            tutor_html=tutor.url,
             width=self.width,
             height=self.height))
+        # read in the template js
         config = self.resource_string("static/js/CTATConfig.js")
+        # fill in the js template
         frag.add_javascript(config.format(
             logtype=self.logtype,
             log_url=self.log_url,
             log_dataset=self.log_dataset,
-            problem_name=self.log_name, #FIXME
+            problem_name=self.log_name,  # FIXME
             question_file=self.brd,
             student_id=self.runtime.anonymous_student_id
             if hasattr(self.runtime, 'anonymous_student_id')
-            else 'bogus-sdk-id',
+            else 'bogus-sdk-id',  # conditional here in case testing in sdk
             saved_state=self.saveandrestore,
             skills=self.skillstring,
             completed=self.completed,
@@ -146,8 +167,10 @@ class CTATXBlock(XBlock):
             # usage_id probably should be parsed. (example:
             # "block-v1:CMU+Stat001+2016+type@ctatxblock+block@ccd1ca4028e64467965c23d8ffbd1363")
             guid=str(uuid.uuid4())))
+        # Add javascript initialization code
         frag.add_javascript(self.resource_string(
             "static/js/Initialize_CTATXBlock.js"))
+        # Execute javascript initialization code
         frag.initialize_js('Initialize_CTATXBlock')
         return frag
 
@@ -165,14 +188,18 @@ class CTATXBlock(XBlock):
         """
         self.attempted = True
         corrects = 0
-        if data.get('value') is not None:
-            corrects = int(data.get('value'))
-            if math.isnan(corrects): corrects = 0  # check for invalid value
-        if data.get('max_value') is not None:
-            max_val = int(data.get('max_value'))
-            if not math.isnan(max_val) and max_val > 0:
-                # only update if a valid number
-                self.max_problem_steps = max_val
+        try:
+            if data.get('value') is not None:
+                corrects = int(data.get('value'))
+                if math.isnan(corrects):
+                    corrects = 0  # check for invalid value
+            if data.get('max_value') is not None:
+                max_val = int(data.get('max_value'))
+                if not math.isnan(max_val) and max_val > 0:
+                    # only update if a valid number
+                    self.max_problem_steps = max_val
+        except ValueError as int_err:
+            return {'result': 'fail', 'error': unicode(int_err)}
         # only change score if it increases.
         # this is done because corrects should only ever increase and
         # it deals with issues EdX has with grading, in particular
@@ -191,7 +218,7 @@ class CTATXBlock(XBlock):
                 self.runtime.publish(self, 'grade', event_data)
             except Exception as err:
                 # return with the error message for debugging purposes.
-                return {'result': 'fail', 'Error': err.message}
+                return {'result': 'fail', 'error': unicode(err)}
             return {'result': 'success', 'finished': self.completed,
                     'score': scaled}
             # pylint: enable=broad-except
@@ -201,18 +228,98 @@ class CTATXBlock(XBlock):
                 'score': float(self.score)/float(self.max_problem_steps)}
 
     def studio_view(self, dummy_context=None):
-        """" Generate what is seen in the Studio view """
+        """" Generate what is seen in the Studio view Edit dialogue. """
+        # read in template
         html = self.resource_string("static/html/ctatstudio.html")
-        frag = Fragment(html.format(self=self))
+        # Make fragment and fill out template
+        frag = Fragment(html.format(
+            src=self.src,
+            brd=self.brd,
+            width=self.width,
+            height=self.height,
+            log_url=self.log_url,
+            dataset=self.log_dataset,
+            log_name=self.log_name,
+            custom=self.custom_tutor_parameters,
+            logging='checked' if self.logtype == "ClientToService" else ''))
+        # read in, add, and execute javascript
         studio_js = self.resource_string("static/js/ctatstudio.js")
         frag.add_javascript(unicode(studio_js))
         frag.initialize_js('CTATXBlockStudio')
         return frag
 
+    @staticmethod
+    def validate_interface(url):
+        """ Validate that the passed url is a CTAT tutor interface. """
+        if url is None:
+            raise Exception('No html interface file specified.')
+        src = bleach.clean(url.strip(), strip=True)
+        tutor = requests.get(src, params={'mode': 'XBlock'})
+        tutor.raise_for_status()
+        if tutor.headers['Content-Type'] != 'text/html':
+            raise Exception(src + ' does not appear to be a text/html file.')
+        if not re.search(r'ctat\.min\.js', tutor.text, re.I):
+            raise Exception(src + ' does not appear to be a CTAT interface.')
+        return src
+
+    @staticmethod
+    def validate_brd(url):
+        """ Validate that the passed url is a CTAT BRD file. """
+        if url is None:
+            raise Exception('No BRD file specified.')
+        brd = bleach.clean(url.strip(), strip=True)
+        tutor = requests.get(brd)
+        tutor.raise_for_status()
+        if not re.search(r'<stateGraph', tutor.text):
+            raise Exception(brd + ' does not appear to be a proper BRD file.')
+        return brd
+
+    @staticmethod
+    def validate_number(num, default):
+        """ Validate that the passed string is a number. """
+        if num is None:
+            return default
+        return int(num)
+
+    @staticmethod
+    def validate_logging(enable_logging):
+        """Validate that the string is a proper value for enabling logging."""
+        logging = "None"
+        if enable_logging is not None:
+            logging = bleach.clean(enable_logging, strip=True)
+            if logging.lower() == "true":
+                logging = "ClientToService"
+        return logging
+
+    @staticmethod
+    def validate_log_param(data, param, logging, default):
+        """ Validate the given logging parameter if logging is enabled. """
+        log_param = default
+        if data.get(param) is not None:
+            log_param = bleach.clean(data.get(param), strip=True)
+        if logging != "None":
+            if len(log_param) <= 0:
+                raise Exception('No specified ' + param +
+                                ' when logging is enabled.')
+        return log_param
+
+    @staticmethod
+    def validate_custom(json_string):
+        """ Validate that the string is JSON. """
+        custom = ''
+        if json_string is not None:
+            # bleach the string
+            custom = bleach.clean(json_string, strip=True)
+            # attempt to parse it as json to see if it is valid.
+            # relying on this to raise a ValueError if not proper json.
+            json.loads(custom)
+        return custom
+
     @XBlock.json_handler
     def studio_submit(self, data, dummy_suffix=''):
         """
         Called when submitting the form in Studio.
+        This will only modify values if all of the safty checks pass.
 
         Args:
           self: the CTAT XBlock.
@@ -222,20 +329,40 @@ class CTATXBlock(XBlock):
         Returns:
           A JSON object reporting the success of the operation.
         """
-        if data.get('src') is not None:
-            self.src = data.get('src')
-        if data.get('brd') is not None:
-            self.brd = data.get('brd')
-        if data.get('width') is not None and not math.isnan(int(data.get('width'))):
-            self.width = int(data.get('width'))
-        if data.get('height') is not None and not math.isnan(int(data.get('height'))):
-            self.height = int(data.get('height'))
-        if data.get('dataset') is not None:
-            self.log_dataset = data.get('dataset')
-        if data.get('logtype') is not None:
-            self.logtype = data.get('logtype')
-        if data.get('logurl') is not None:
-            self.log_url = data.get('logurl')
+        # pylint: disable=broad-except
+        # This uses the generic exceptions so that we do not have to
+        # generate custom error classes and this handles the classes
+        # of exceptions potentially raised by requests
+        try:
+            valid_src = self.validate_interface(data.get('src'))
+            valid_brd = self.validate_brd(data.get('brd'))
+            valid_width = self.validate_number(data.get('width'), self.width)
+            valid_height = self.validate_number(data.get('height'),
+                                                self.height)
+            valid_logtype = self.validate_logging(data.get('logging'))
+            valid_log_url = self.validate_log_param(data, 'logserver',
+                                                    valid_logtype,
+                                                    self.log_url)
+            valid_log_dataset = self.validate_log_param(data, 'dataset',
+                                                        valid_logtype,
+                                                        self.log_dataset)
+            valid_log_name = self.validate_log_param(data, 'problemname',
+                                                     valid_logtype,
+                                                     self.log_name)
+            valid_custom = self.validate_custom(data.get('custom'))
+        except Exception as err:
+            return {'result': 'fail', 'error': unicode(err)}
+        # pylint: enable=broad-except
+        # Only update values if all checks pass
+        self.src = valid_src
+        self.brd = valid_brd
+        self.width = valid_width
+        self.height = valid_height
+        self.logtype = valid_logtype
+        self.log_url = valid_log_url
+        self.log_dataset = valid_log_dataset
+        self.log_name = valid_log_name
+        self.custom_tutor_parameters = valid_custom
         return {'result': 'success'}
 
     @XBlock.json_handler
@@ -254,7 +381,7 @@ class CTATXBlock(XBlock):
           success status.
         """
         if data.get('state') is not None:
-            self.saveandrestore = data.get('state')
+            self.saveandrestore = bleach.clean(data.get('state'))
             return {'result': 'success'}
         return {'result': 'failure'}
 
@@ -272,7 +399,7 @@ class CTATXBlock(XBlock):
         """
         # FIXME: actually insert hooks in code to send this.
         if data.get('skills') is not None:
-            self.skillstring = data.get('skills')
+            self.skillstring = bleach.clean(data.get('skills'), strip=True)
             return {'result': 'success'}
         return {'result': 'failure'}
 
@@ -296,7 +423,7 @@ class CTATXBlock(XBlock):
         return [
             ("CTATXBlock",
              """<vertical_demo>
-                <ctatxblock width="" height=""/>
+                <ctatxblock width="690" height="550"/>
                 </vertical_demo>
              """),
         ]
--- ctatxblock/static/js/ctatstudio.js
+++ ctatxblock/static/js/ctatstudio.js
@@ -9,11 +9,70 @@ function CTATXBlockStudio(runtime, element)
 	    src: $(element).find('input#interface').val(),
 	    brd: $(element).find('input#brd').val(),
 	    width: $(element).find('input#maxwidth').val(),
-	    height: $(element).find('input#maxheight').val()
+	    height: $(element).find('input#maxheight').val(),
+	    logging: $(element).find('input#logging').is(':checked'),
+	    logserver: $(element).find('input#log_url').val(),
+	    dataset: $(element).find('input#log_dataset').val(),
+	    problemname: $(element).find('input#log_name').val(),
+	    custom: $(element).find('textarea#extra').val()
 	};
-	runtime.notify('save', {state: 'start'});
+	if (data.src.trim().length <= 0) {
+	    alert('Interface file needs to be not empty.');
+	    return;
+	}
+	if (data.brd.trim().length <=0) {
+	    alert('BRD file needs to be not empty.');
+	    return;
+	}
+	var w = Number(data.width);
+	if (isNaN(w) || w<100) {
+	    alert('Width should be at least 100.');
+	    return;
+	}
+	var h = Number(data.height);
+	if (isNaN(h) || h<100) {
+	    alert('Height should be at least 100.');
+	    return;
+	}
+	if (data.logging) {
+	    if (data.logserver.trim().length <= 0) {
+		alert('When logging is enabled, a log service needs to be specified.');
+		return;
+	    }
+	    if (data.dataset.trim().length <= 0) {
+		alert('When logging is enabled, a dataset name needs to be specified.');
+		return;
+	    }
+	    if (data.problemname.trim().length <= 0) {
+		alert('When logging is enabled, a problem name needs to be specified.');
+		return;
+	    }
+	}
+	if (data.custom) {
+	    try {
+		JSON.parse(data.custom);
+	    } catch(err) {
+		alert('Invalid JSON in Extra Parameters: ' + err);
+		return;
+	    }
+	}
+	console.log(runtime);
+	if (runtime.notify)
+	    runtime.notify('save', {state: 'start'});
 	$.post(handlerUrl, JSON.stringify(data)).done(function(response) {
-	    runtime.notify('save', {state: 'end'});
+	    if (response['result'] != 'success') {
+		console.log(response);
+		if (response['error']) {
+		    alert(response['error']);
+		} else {
+		    alert('Save failed!');
+		}
+	    }
+	}).fail(function() {
+	    alert("Error in posting configuration parameters!");
+	}).always(function() {
+	    if (runtime.notify)
+		runtime.notify('save', {state: 'end'});
 	});
     });
 
